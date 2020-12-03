9cbfe76ea3a2bebc30bc2b69df784500325fb5a3
Moomal <moomalmsoomro@gmail.com>
2020-12-02 22:43:56 -0500

    commit tree 629b8e4f1eaab4092aea7af846498b4f1456e1ba

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    @@genres = []
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    
+    def initialize (title)
+        @title = title
+        @@genres << genre if !@@genres.include?(genre)
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
